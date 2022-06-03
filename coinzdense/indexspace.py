6a2603f2988941fb7cf9a7f04ccbd54eb142734b
Rob Meijer <pibara@gmail.com>
2022-06-03 23:57:42 +0200

    start at refactor indexspace

--- coinzdense/indexspace.py
+++ coinzdense/indexspace.py
@@ -1,39 +1,46 @@
 #!/usr/bin/python3
 
 class IndexSpace:
-    def __init__(self, offset=0, exponent=64, reserved_exp=0, state=None):
-        assert isinstance(offset, int)
-        assert offset >= 0
-        assert offset < 1 << 64 - 1024
-        assert isinstance(exponent, int)
-        assert exponent >= 10
-        assert exponent <= 64
-        assert isinstance(reserved_exp, int)
-        assert reserved_exp == 0 or reserved_exp >= 6
-        assert reserved_exp <= exponent - 16
-        self.heap = offset + 1 << exponent
-        assert self.heap <= 1 << 64
-        self.offset = offset
-        self.has_reserved = False
-        self.stack = 0
-        self.stack_reserved = 0
-        if reserved_exp:
-            self.stack = 1 << reserved_exp
-            self.has_reserved = True
-        if state not is None:
-            assert isinstance(state, list)
-            assert len(state) == 3
-            self.heap = state[0]
-            self.stack = state[1]
-            self.stack_reserved = state[2]
-            assert isinstance(self.heap, int)
-            assert self.heap
-            assert isinstance(self.stack, int)
-            assert isinstance(self.stack_reserved, int)
-            assert self.heap <= offset + 1 << exponent
-            assert self.heap > self.stack
-            assert not self.has_reserved or self.stack_reserved < self.stack
-    
-
+    def __init__(self, offset=0, size=1<<64, reserved=0, state=None):
+        if state is None:
+            if reserved == 0:
+                self.has_reserved = False
+                self.main_offset = offset
+                self.main_heap = offset
+                self.reserved_offset = offset
+                self.reserved_heap = offset
+            else:
+                self.has_reserved = True
+                self.reserved_offset = offset
+                self.reserved_heap = offset
+                self.main_offset = reserved + offset
+                self.main_heap = reserved + offset
+            self.stack = size + offset
+        else:
+            self.has_reserved = state[0]
+            self.reserved_offset = state[1]
+            self.reserved_heap = state[2]
+            self.main_offset = state[3]
+            self.main_heap = state[4]
+            self.stack = state[5]
+    def key_allocate(self, index_chunk_size):
+        if self.main_heap + index_chunk_size > self.stack:
+            raise RuntimeError("Index-apce non-reserved heap exausted")
+        offset = self.main_heap
+        self.main_heap += index_chunk_size
+        return offset
+    def subkey_allocate(self, index_chunk_size, subkey_keyspace):
+        offset2 = self.stack - subkey_keyspace
+        if offset2 < self.main_heap:
+            raise RuntimeError("Index-space stack exausted")
+        if self.reserved_heap + index_chunk_size > self.main_offset:
+            if self.main_heap + index_chunk_size > offset2:
+                raise RuntimeError("Index-space full heap exausted")
+            offset = self.main_heap
+            self.main_heap += index_chunk_size
+        else:
+            offset = self.reserved_heap
+            self.reserved_heap += index_chunk_size
+        return [offset, offset2]
 
 
